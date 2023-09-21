264a61fe73a7c5eb82d6e6a53b5ca9469916733f
fezarosa_dev <fezarosa.dev@gmail.com>
2023-09-21 01:19:03 -0300

    add a new funcitions, creat a json and xlsx

--- funcoes/dados/__init__.py
+++ funcoes/dados/__init__.py
@@ -1,18 +1,30 @@
-from openpyxl import Workbook
+import openpyxl
+import json
 
 
-def criar_arquivo_xlsx_com_colunas(nome_arquivo, nomes_colunas):
-    try:
-        workbook = Workbook()
-        sheet = workbook.active
+def makexlsx(file_name):
+    # Read the JSON file
+    with open('people.json', 'r') as json_file:
+        json_data = json.load(json_file)
 
-        # Inserir os nomes das colunas na primeira linha da planilha
-        for idx, nome_coluna in enumerate(nomes_colunas, start=1):
-            sheet.cell(row=1, column=idx, value=nome_coluna)
+    # Create a new XLSX file
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
 
-        # Salvar o arquivo XLSX
-        workbook.save(nome_arquivo)
+    # Check if there is data in the JSON
+    if json_data:
+        # Add a header with the keys from the first dictionary in the JSON as column names
+        header = list(json_data[0].keys())
+        sheet.append(header)
 
-        print(f"Arquivo '{nome_arquivo}' criado com sucesso com as colunas: {', '.join(nomes_colunas)}")
-    except Exception as e:
-        print(f"Erro ao criar o arquivo XLSX: {str(e)}")
+        # Add JSON data to the spreadsheet
+        for row in json_data:
+            data = list(row.values())
+            sheet.append(data)
+
+        # Save the spreadsheet to an XLSX file
+        workbook.save(file_name)
+
+        print("Planilha criada com sucesso.")
+    else:
+        print("O JSON está vazio. Não foi criada nenhuma planilha.")
--- main.py
+++ main.py
@@ -1,66 +1,131 @@
+import json
+import logging
+import math
 import tkinter as tk
 import tkinter.simpledialog as simpledialog
+
 from funcoes import dados
 from funcoes import terminal
-from time import sleep
-import logging
 
-# Solicitar o nome do arquivo
-file_name = simpledialog.askstring("Nome do Arquivo", "Digite o nome do arquivo:")
-file_name.strip()
-if file_name[-4:] != 'xlsx':
-    file_name = file_name + '.xlsx'
+planilha = bool()
 
-counter = 1
-terminal.titulo('Digite tudo que se pede: ', terminal.Cores.roxo)
-while True:
-    informations_dict = {}
 
+def pegar_dados():
+    global planilha
+    root.destroy()  # Fecha a janela
+    planilha = False
+
+
+def gerar_planilhas():
+    global planilha
+    root.destroy()  # Fecha a janela
+    planilha = True
+
+
+root = tk.Tk()
+root.title("Selecione uma Opção")
+
+label = tk.Label(root, text="Escolha uma opção:")
+label.pack()
+
+pegar_button = tk.Button(root, text="Pegar Dados", command=pegar_dados)
+pegar_button.pack()
+
+gerar_button = tk.Button(root, text="Gerar Planilhas com Dados Existentes", command=gerar_planilhas)
+gerar_button.pack()
+
+root.mainloop()
+
+if planilha:
+    # Solicitar o nome do arquivo
+    file_name_xlsx = simpledialog.askstring("Nome do Arquivo XLSX", "Digite o nome do arquivo XLSX:")
+    file_name_xlsx.strip()
+
+    if file_name_xlsx[-4:] != 'xlsx':
+        file_name_xlsx = file_name_xlsx + '.xlsx'
+    dados.makexlsx(file_name_xlsx)
+
+else:
+    # Abrir o arquivo XLSX no modo de anexação (não alterar essa parte)
+    # ...
+
+    # Nome do arquivo JSON
+    file_name_json = 'people.json'
+
+    # Lista para armazenar todas as informações coletadas
+    informations_list = []
+    counter = 1
+    terminal.titulo('Digite tudo que se pede: ', terminal.Cores.roxo)
     while True:
-        name = input('Digite o nome: ')
-        if name.replace(' ', '').isalpha():
+        informations_dict = {}
+
+        while True:
+            name = input('Digite o nome: ')
+            if name.replace(' ', '').isalpha():
+                break
+
+        while True:
+            age = input('Digite a idade: ')
+            if age.isdigit():
+                age = float(age)
+                break
+
+        while True:
+            gender = input('Digite o gênero (M para masculino e F para feminino): ')
+            if gender.isalpha() and gender.lower() in 'mf':
+                gender = gender.lower()
+                break
+
+        while True:
+            weight = input('Digite o peso: ')
+            if weight.replace(',', '', 1).replace('.', '', 1).isdigit():
+                weight = float(weight.replace(',', '.', 1))
+                break
+
+        while True:
+            height = input('Digite a altura (use ponto para separar decimais, por exemplo, 1.75): ')
+            if height.replace(',', '', 1).replace('.', '', 1).replace(' ', '').replace('m', '').isdigit():
+                height = float(height.replace(',', '.', 1).replace('m', ''))
+                if height > 3:
+                    height = height / 100
+                break
+        imc = weight / math.pow(height, 2)
+
+        if imc < 18.5:
+            imcstatus = "Abaixo do peso"
+        elif 18.5 <= imc < 25.0:
+            imcstatus = "Peso saudável"
+        elif 25.0 <= imc < 30.0:
+            imcstatus = "Sobrepeso"
+        elif 30.0 <= imc < 35.0:
+            imcstatus = "Obesidade grau 1"
+        elif 35.0 <= imc < 40.0:
+            imcstatus = "Obesidade grau 2"
+        else:
+            imcstatus = "Obesidade grau 3 (obesidade mórbida)"
+        informations_dict = {
+            'name': name,
+            'age': age,
+            'gender': gender,
+            'weight': weight,
+            'height': height,
+            'imc': imc,
+            'imcStatus': imcstatus,
+        }
+
+        # Adicionar o dicionário de informações à lista
+        informations_list.append(informations_dict)
+        terminal.titulo(
+        terminal.titulo(f"{informations_dict['name']} numero: {counter}, tem o imc de {informations_dict['imc']}, sendo {informations_dict['imcStatus']}"))
+        stop = input('Deseja parar? (S para parar): ')
+        if stop.lower() == 's':
             break
 
-    while True:
-        age = input('Digite a idade: ')
-        if age.isdigit():
-            age = float(age)
-            break
+        counter += 1
 
-    while True:
-        gender = input('Digite o gênero (M para masculino e F para feminino): ')
-        if gender.isalpha() and gender.lower() in 'mf':
-            gender = gender.lower()
-            break
+    logging.basicConfig(filename='quantity.log', level=logging.INFO, format='%(message)s', filemode='w')
+    logging.info(counter)
 
-    while True:
-        weight = input('Digite o peso: ')
-        if weight.replace(',', '', 1).replace('.', '', 1).isdigit():
-            weight = float(weight.replace(',', '.', 1))
-            break
-
-    while True:
-        height = input('Digite a altura (use ponto para separar decimais, por exemplo, 1.75): ')
-        if height.replace(',', '', 1).replace('.', '', 1).replace(' ', '').replace('m', '').isdigit():
-            height = float(height.replace(',', '.', 1).replace('m', ''))
-            if height > 3:
-                height = height / 100
-            break
-
-    informations_dict = {
-        'name': name,
-        'age': age,
-        'gender': gender,
-        'weight': weight,
-        'height': height,
-    }
-
-    stop = input('Deseja parar? (S para parar): ')
-    if stop.lower() == 's':
-        break
-
-    counter += 1
-
-logging.basicConfig(filename='quantity.log', level=logging.INFO, format='%(message)s')
-logging.info(counter)
-print(informations_dict)
+    # Salvar a lista de informações em um arquivo JSON
+    with open(file_name_json, 'a') as json_file:
+        json.dump(informations_list, json_file)
